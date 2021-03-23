b02c49569ad91e7e25ce8556b8f149caeb9c21f4
Gustavo Malheiro <gus.malheiro@gmail.com>
2021-03-22 22:05:48 -0300

    sending forms to node

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ app.get("/perguntar", (req, res) => {
     res.render("perguntar"); // renderizando um html (mais especificamente, a view perguntar)
 });
 
+// rota para receber dados do formulário
+app.post("/salvarpergunta", (req, res) => { // o formulário trabalha com POST, então a rota também deve ser post. 
+    res.send("Formulário recebido!")
+});
+
 // rodando a aplicação
 app.listen(8080, () => {
     console.log("App rodando!");
