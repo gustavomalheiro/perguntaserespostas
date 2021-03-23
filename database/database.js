4c4eb40e372bb8ede8b53145a7b17a0669fc791e
gustavomalheiro <gus.malheiro@gmail.com>
2021-03-23 14:00:35 -0300

    Delete database.js

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('guiaperguntas', 'root', '66k923200', {
-    host: 'localhost', // onde está rodando o mysql
-    dialect: 'mysql' // qual tipo de bd queremos nos conectar
-});
-
-module.exports = connection;
